a9550ee3caba5b0c0860138619a4cf83a0784cca
nguyenduong <duongynguyen0501@gmail.com>
2019-08-01 10:56:53 +0700

    building the application shell

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+import { RouterConfiguration, Router } from 'aurelia-router';
+import { PLATFORM } from "aurelia-pal";
+
 export class App {
-  public message: string = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    this.router = router;
+    config.title = 'Contacts';
+    config.map([
+      { route: '', moduleId: PLATFORM.moduleName('no-selection'), title: 'Select' },
+      { route: 'contacts/:id', name: 'contacts', moduleId: PLATFORM.moduleName('contact-detail') }
+    ]);
+  }
 }
